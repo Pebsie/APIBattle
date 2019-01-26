ce2a4014b2050599ac2ecca17ec9d245d52e6821
Thomas Lock <email@peb.si>
2019-01-26 22:05:50 +0000

    Alter world generation

--- examples/view.php
+++ examples/view.php
@@ -1,15 +1,3 @@
 <?php
-    $world_json = http_get("../api.php?a=get&scope=world&type=buildings");
 
-    $world = json_decode($world_json);
-
-    $i = 1;
-    foreach ($world as $tile) {
-        echo "<img src='http://peb.si/b/img/".$world['buildingType'].".png' />";
-        $i++;
-        if ($i > 100) {
-            echo "<br />";
-            $i = 1;
-        }
-    }
 ?>
--- modules/generateWorld.php
+++ modules/generateWorld.php
@@ -3,10 +3,11 @@
     require "../editor.php";
 
     for ($i=1; $i <= 100*100; $i++) {
- 
-        if (rand(1,5) == 1) {
+        build($pdo, "Grass", "Mother Nature", $i, NULL);
+        
+        if (rand(1,10) == 1) {
             build($pdo, "Forest", "Mother Nature", $i, NULL);
-        } elseif (rand(1,5) == 1) {
+        } elseif (rand(1,250) == 1) {
             build($pdo, "Mine", "Mother Nature", $i, NULL);
         }
 
