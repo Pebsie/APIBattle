aab7258d464ca22ea2844695242fe2a04af6b353
Thomas Lock <email@peb.si>
2019-01-26 16:58:28 +0000

    Fix error in sql queries in editor functions

--- editor.php
+++ editor.php
@@ -3,7 +3,7 @@
     This file contains functions that are useful for adding things to the base game such as buildings. */
 
     function addBuilding($pdo, $buildingType, $goldCost, $woodCost, $stoneCost, $timeToBuild, $timeToDeposit, $attribute, $depositValue, $hp, $requirement) {
-        $sql = "INSERT INTO buildings (buildingType, goldCost, woodCost, stoneCost, timeToBuild, timeToDeposit, attribute, depositValue, hp, requirement) VALUES ('".$buildingType."', ".$goldCost.", ".$woodCost.", ".$stoneCost.", ".$timeToBuild.", ".$timeToDeposit.", '".$attribute."', ".$depositValue.", ".$hp.", '".$requirement."'));";
+        $sql = "INSERT INTO buildings (buildingType, goldCost, woodCost, stoneCost, timeToBuild, timeToDeposit, attribute, depositValue, hp, requirement) VALUES ('".$buildingType."', ".$goldCost.", ".$woodCost.", ".$stoneCost.", ".$timeToBuild.", ".$timeToDeposit.", '".$attribute."', ".$depositValue.", ".$hp.", '".$requirement."');";
         $query = $pdo->prepare($sql);
         $query->execute();
         echo "Added ".$buildingType." <br />";
@@ -15,7 +15,7 @@
         $query->execute();
         $result = $query->fetch(PDO::FETCH_ASSOC);
         
-        $sql = "INSERT INTO building (buildingType, hp, position, username, special) VALUES ('".$buildingType."', ".$row['hp'].", ".$position.", '".$owner."', '".$special."'));";
+        $sql = "INSERT INTO building (buildingType, hp, position, username, special) VALUES ('".$buildingType."', ".$row['hp'].", ".$position.", '".$owner."', '".$special."');";
         $query = $pdo->prepare($sql);
         $query->execute();
     }
