d61558248104b843e25bce535a51caa8d831c4be
Thomas Lock <email@peb.si>
2019-01-27 12:13:12 +0000

    fix generateWorld algorithm using too few arguments for build

--- modules/generateWorld.php
+++ modules/generateWorld.php
@@ -9,9 +9,9 @@
         $query->execute();
 
         if (rand(1,10) == 1) {
-            build($pdo, "Forest", "Mother Nature", $i, NULL);
+            build($pdo, "Forest", "Mother Nature", $i, NULL, 0);
         } elseif (rand(1,250) == 1) {
-            build($pdo, "Mine", "Mother Nature", $i, NULL);
+            build($pdo, "Mine", "Mother Nature", $i, NULL, 0);
         }
 
     }
