7114dced5b29f6149777fe4246ba9d813aa17583
Thomas Lock <email@peb.si>
2019-01-30 14:56:24 +0000

    fix few too arguments error on baseBuildings'

--- modules/baseBuildings.php
+++ modules/baseBuildings.php
@@ -1,10 +1,12 @@
 <?php
     require "../connect.php";
     require "../editor.php";
-    addBuilding($pdo, 'Castle', 1, 1, 1, 1, 1, "unit", 10, 5, "God");
-    addBuilding($pdo, 'House', 1, 5, 1, 1, 5, "pop", 1, 1, NULL);
-    addBuilding($pdo, 'Barracks', 3, 10, 5, 5, 10, "unit", 1, 10, "House");
+    addBuilding($pdo, 'Castle', 1, 1, 1, 1, 1, "unit", 10, 5, "God", "false");
+    addBuilding($pdo, 'House', 1, 5, 1, 1, 5, "pop", 1, 1, NULL, "false");
+    addBuilding($pdo, 'Barracks', 3, 10, 5, 5, 10, "unit", 1, 10, "House", "false");
     addBuilding($pdo, 'Farm', 3, 3, 5, 2, 5, "food", 2, 5, "House");
-    addBuilding($pdo, 'Mine', 100, 50, 100, 30, 10, "stone", 10, 100, "God");
-    addBuilding($pdo, 'Forest', 100, 100, 100, 3, 10, "wood", 3, 20, "God");
+    addBuilding($pdo, 'Mine', 100, 50, 100, 30, 10, "stone", 10, 100, "God", "false");
+    addBuilding($pdo, 'Forest', 100, 100, 100, 3, 10, "wood", 3, 20, "God", "false");
+    addBuilding($pdo, 'Wall', 1, 1, 1, 10, 1, "unit", 100, 5, "Barracks", "false");
+    addBuilding($pdo, 'Skeleton', 1, 1, 1, 1, 1, "unit", 20, 5, "God", true, "false");
 ?>
--- modules/specialBuildings.php
+++ modules/specialBuildings.php
@@ -1,6 +1,5 @@
 <?php
     require "../connect.php";
     require "../editor.php";
-    addBuilding($pdo, 'Wall', 1, 1, 1, 10, 1, "unit", 100, 5, "Barracks");
-    addBuilding($pdo, 'Skeleton', 1, 1, 1, 1, 1, "unit", 20, 5, "God", true);
+   
 ?>
