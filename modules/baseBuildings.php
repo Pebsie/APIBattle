654b1863de393f010c29e97857b9c203f72f080d
Thomas Lock <email@peb.si>
2019-01-31 14:27:30 +0000

    make world generation cause forests to build together

--- modules/baseBuildings.php
+++ modules/baseBuildings.php
@@ -9,6 +9,6 @@
     addBuilding($pdo, 'Farm', 3, 3, 5, 2, 5, "food", 2, 5, "House", "false");
     addBuilding($pdo, 'Mine', 100, 50, 100, 30, 10, "stone", 10, 100, "God", "false");
     addBuilding($pdo, 'Forest', 100, 100, 100, 3, 10, "wood", 3, 20, "God", "false");
-    addBuilding($pdo, 'Wall', 1, 1, 1, 10, 1, "unit", 100, 5, "Barracks", "false");
+    addBuilding($pdo, 'Wall', 1, 1, 1, 10, 1, "unit", 100, 5, "Barracks", "true");
     addBuilding($pdo, 'Skeleton', 1, 1, 1, 1, 1, "unit", 20, 5, "God", "false");
 ?>
--- modules/generateWorld.php
+++ modules/generateWorld.php
@@ -1,19 +1,24 @@
 <?php
     require "../connect.php";
     require "../editor.php";
-
+    $log = array();
     echo "Generating world... ";
     for ($i=1; $i <= 100*100; $i++) {
         $sql = "INSERT INTO world (buildingType, username, units) VALUES ('Grass', 'Mother Nature', 0);";
         $query = $pdo->prepare($sql);
         $query->execute();
 
-        if (rand(1,10) == 1) {
+        if (($log[i-1] == "Forest" || $log[i-100] == "Forest") && rand(1,4) == 1) {
             build($pdo, "Forest", "Mother Nature", $i, NULL, 0);
+        } elseif (rand(1,200) == 1) {
+            build($pdo, "Forest", "Mother Nature", $i, NULL, 0);
+            $log[i] = "Forest";
         } elseif (rand(1,500) == 1)  {
             build($pdo, "Skeleton", "Dark World", $i, NULL, rand(100,350));
+            $log[i] = "Skeleton";
         } elseif (rand(1,250) == 1) {
             build($pdo, "Mine", "Mother Nature", $i, NULL, 0);
+            $log[i] = "Mine";
         }
 
     }
