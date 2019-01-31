dba4bcbd9aa1682829c45a6b769efec2e0b5f299
Thomas Lock <email@peb.si>
2019-01-31 14:55:09 +0000

    make skeletons group together in world generation

--- modules/generateWorld.php
+++ modules/generateWorld.php
@@ -8,17 +8,21 @@
         $query = $pdo->prepare($sql);
         $query->execute();
 
-        if (($log[$i-1] == "Forest" || $log[$i-100] == "Forest") && rand(1,4) == 1) {
+        if (($log[$i-1] == "Forest" || $log[$i-100] == "Forest") && rand(1,100) < 35) {
             build($pdo, "Forest", "Mother Nature", $i, NULL, 0);
-        } elseif (rand(1,200) == 1) {
-            build($pdo, "Forest", "Mother Nature", $i, NULL, 0);
-            $log[i] = "Forest";
-        } elseif (rand(1,500) == 1)  {
+            $log[$i] = "Forest";
+        } elseif (($log[$i-1] == "Skeleton" || $log[$i-100] == "Skeleton") && rand(1,100) < 50) {
             build($pdo, "Skeleton", "Dark World", $i, NULL, rand(100,350));
-            $log[i] = "Skeleton";
+            $log[$i] = "Skeleton";
+        } elseif (rand(1,10) == 1) {
+            build($pdo, "Forest", "Mother Nature", $i, NULL, 0);
+            $log[$i] = "Forest";
+        } elseif (rand(1,1500) == 1)  {
+            build($pdo, "Skeleton", "Dark World", $i, NULL, rand(100,350));
+            $log[$i] = "Skeleton";
         } elseif (rand(1,250) == 1) {
             build($pdo, "Mine", "Mother Nature", $i, NULL, 0);
-            $log[i] = "Mine";
+            $log[$i] = "Mine";
         }
 
     }
